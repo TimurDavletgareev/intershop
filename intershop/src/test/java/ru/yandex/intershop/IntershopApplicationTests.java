c4a0c94d041c356712a88d7a09e6bfd6f92cc13e
TimurDV88 <kumba-yo@yandex.ru>
2025-03-25 13:28:15 +0530

    add docker

--- intershop/src/test/java/ru/yandex/intershop/IntershopApplicationTests.java
+++ intershop/src/test/java/ru/yandex/intershop/IntershopApplicationTests.java
@@ -2,8 +2,10 @@ package ru.yandex.intershop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IntershopApplicationTests {
 
 	@Test
