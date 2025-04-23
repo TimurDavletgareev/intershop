9f99273599a15c4001e2e5707bec8a44ef3f86f6
TimurDV88 <kumba-yo@yandex.ru>
2025-04-23 19:57:32 +0530

    fix compiler

--- main-service/src/test/java/ru/yandex/intershop/IntershopApplicationTests.java
+++ main-service/src/test/java/ru/yandex/intershop/IntershopApplicationTests.java
@@ -1,17 +1,15 @@
 package ru.yandex.intershop;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@Disabled
 class IntershopApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
