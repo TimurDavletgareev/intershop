277f5b8970d51a6af9c2debf4c7218f387b31902
TimurDV88 <kumba-yo@yandex.ru>
2025-06-15 09:12:56 +0500

    add security config

---
+++ main-service/src/main/java/ru/yandex/intershop/config/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package ru.yandex.intershop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf().disable()
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
+                )
+                .formLogin(withDefaults())
+                /* Вход через OAuth 2.0 провайдеров
+                 .oauth2Login()
+                */
+                .logout(logout -> logout.logoutUrl("/"))
+                .exceptionHandling(handling -> handling
+                        .accessDeniedHandler((exchange, denied) ->
+                                Mono.error(new AccessDeniedException("Access Denied")))
+                )
+                .build();
+    }
+}
