9a8b440911a8ab2b85e7ce4bf4c3583df765a7d6
TimurDV88 <kumba-yo@yandex.ru>
2025-06-18 11:26:02 +0500

    add logout logic

--- main-service/src/main/java/ru/yandex/intershop/config/SecurityConfiguration.java
+++ main-service/src/main/java/ru/yandex/intershop/config/SecurityConfiguration.java
@@ -2,18 +2,15 @@ package ru.yandex.intershop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.logout.HttpStatusReturningServerLogoutSuccessHandler;
+import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Mono;
-import ru.yandex.intershop.service.RoleService;
-import ru.yandex.intershop.service.UserService;
-import ru.yandex.intershop.service.entity.UserEntityService;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -26,7 +23,7 @@ public class SecurityConfiguration {
         return http
                 .csrf().disable()
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/", "/main/items").permitAll()
+                        .pathMatchers("/", "/main/**", "items/*").permitAll()
                         .anyExchange().authenticated()
                 )
                 .formLogin(withDefaults())
@@ -35,7 +32,14 @@ public class SecurityConfiguration {
                 */
                 .logout(logout -> logout
                         .logoutUrl("/logout")
-                        .logoutSuccessHandler(new HttpStatusReturningServerLogoutSuccessHandler())
+                        .logoutSuccessHandler((exchange, authentication) ->
+                                exchange.getExchange().getSession()
+                                        .flatMap(WebSession::invalidate) // удаляем сессию
+                                        .then(Mono.fromRunnable(() -> {
+                                            exchange.getExchange().getResponse()
+                                                    .setStatusCode(HttpStatus.OK); // отвечаем 200 OK
+                                        }))
+                        )
                 )
                 .exceptionHandling(handling -> handling
                         .accessDeniedHandler((exchange, denied) ->
